65c2c33a5f7b6447dc8b16953c0386378afb81bf
Nikitha Nikki <Nikitha.nikki525@gmail.com>
2023-01-20 10:46:40 +0530

    committed

---
+++ src/main.js
@@ -0,0 +1,5 @@
+const file = require("./index");
+// file.myFileWriter("File.txt", "Hello");
+// file.myFileReader("File.txt");
+// file.myFileUpdater("File.txt", " World");
+file.myFileDeleter("File.txt");
